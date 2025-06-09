cd403ea76ccf441d4e80650279d87e55d387fb01
Manuel de la Peña <mdelapenya@gmail.com>
2025-06-09 11:26:48 +0200

    chore: add tests

--- dockercontainer/lifecycle.create_test.go
+++ dockercontainer/lifecycle.create_test.go
@@ -1,11 +1,15 @@
 package dockercontainer
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
 	"github.com/docker/go-connections/nat"
+	"github.com/docker/go-sdk/dockerclient"
 )
 
 func TestMergePortBindings(t *testing.T) {
@@ -95,4 +99,264 @@ func TestMergePortBindings(t *testing.T) {
 			require.Equal(t, c.expected, res)
 		})
 	}
+}
+
+func TestPreCreateModifierHook(t *testing.T) {
+	ctx := context.Background()
+
+	dockerClient, err := dockerclient.New(context.Background())
+	require.NoError(t, err)
+	defer dockerClient.Close()
+
+	t.Run("no-exposed-ports", func(t *testing.T) {
+		def := &Definition{
+			Image: "nginx:alpine", // alpine image does expose port 80
+			ConfigModifier: func(config *container.Config) {
+				config.Env = []string{"a=b"}
+			},
+			HostConfigModifier: func(hostConfig *container.HostConfig) {
+				hostConfig.PortBindings = nat.PortMap{
+					"80/tcp": []nat.PortBinding{
+						{
+							HostIP:   "1",
+							HostPort: "2",
+						},
+					},
+				}
+			},
+			EndpointSettingsModifier: func(endpointSettings map[string]*network.EndpointSettings) {
+				endpointSettings["a"] = &network.EndpointSettings{
+					Aliases: []string{"b"},
+					Links:   []string{"link1", "link2"},
+				}
+			},
+		}
+
+		// define empty inputs to be overwritten by the pre create hook
+		inputConfig := &container.Config{
+			Image: def.Image,
+		}
+		inputHostConfig := &container.HostConfig{}
+		inputNetworkingConfig := &network.NetworkingConfig{}
+
+		err := preCreateContainerHook(ctx, dockerClient, def, inputConfig, inputHostConfig, inputNetworkingConfig)
+		require.NoError(t, err)
+
+		// assertions
+
+		require.True(t, inputHostConfig.PublishAllPorts)
+		require.Equal(
+			t,
+			[]string{"a=b"},
+			inputConfig.Env,
+			"Docker config's env should be overwritten by the modifier",
+		)
+		require.Equal(
+			t,
+			[]string{"b"},
+			inputNetworkingConfig.EndpointsConfig["a"].Aliases,
+			"Networking config's aliases should be overwritten by the modifier",
+		)
+		require.Equal(
+			t,
+			[]string{"link1", "link2"},
+			inputNetworkingConfig.EndpointsConfig["a"].Links,
+			"Networking config's links should be overwritten by the modifier",
+		)
+	})
+
+	t.Run("no-exposed-ports-and-network-mode-is-container", func(t *testing.T) {
+		def := &Definition{
+			Image: "nginx:alpine", // alpine image does expose port 80
+			HostConfigModifier: func(hostConfig *container.HostConfig) {
+				hostConfig.PortBindings = nat.PortMap{
+					"80/tcp": []nat.PortBinding{
+						{
+							HostIP:   "1",
+							HostPort: "2",
+						},
+					},
+				}
+				hostConfig.NetworkMode = "container:foo"
+			},
+		}
+
+		// define empty inputs to be overwritten by the pre create hook
+		inputConfig := &container.Config{
+			Image: def.Image,
+		}
+		inputHostConfig := &container.HostConfig{}
+		inputNetworkingConfig := &network.NetworkingConfig{}
+
+		err := preCreateContainerHook(ctx, dockerClient, def, inputConfig, inputHostConfig, inputNetworkingConfig)
+		require.NoError(t, err)
+
+		// assertions
+
+		require.Equal(
+			t,
+			nat.PortSet(nat.PortSet{}),
+			inputConfig.ExposedPorts,
+			"Docker config's exposed ports should be empty",
+		)
+		require.Equal(t,
+			nat.PortMap{},
+			inputHostConfig.PortBindings,
+			"Host config's portBinding should be empty",
+		)
+	})
+
+	t.Run("definition-contains-more-than-one-network-including-aliases", func(t *testing.T) {
+		networkName := "foo" + t.Name()
+		nw := testCreateNetwork(t, networkName)
+
+		def := &Definition{
+			Image:    "nginx:alpine", // alpine image does expose port 80
+			Networks: []string{networkName, "bar"},
+			NetworkAliases: map[string][]string{
+				networkName: {"foo1"}, // network aliases are needed at the moment there is a network
+			},
+		}
+
+		// define empty inputs to be overwritten by the pre create hook
+		inputConfig := &container.Config{
+			Image: def.Image,
+		}
+		inputHostConfig := &container.HostConfig{}
+		inputNetworkingConfig := &network.NetworkingConfig{}
+
+		err = preCreateContainerHook(ctx, dockerClient, def, inputConfig, inputHostConfig, inputNetworkingConfig)
+		require.NoError(t, err)
+
+		// assertions
+
+		require.Equal(
+			t,
+			def.NetworkAliases[networkName],
+			inputNetworkingConfig.EndpointsConfig[networkName].Aliases,
+			"Networking config's aliases should come from the container request",
+		)
+		require.Equal(
+			t,
+			nw.ID,
+			inputNetworkingConfig.EndpointsConfig[networkName].NetworkID,
+			"Networking config's network ID should be retrieved from Docker",
+		)
+	})
+
+	t.Run("definition-contains-more-than-one-network-without-aliases", func(t *testing.T) {
+		networkName := "foo" + t.Name()
+		nw := testCreateNetwork(t, networkName)
+
+		def := &Definition{
+			Image:    "nginx:alpine", // alpine image does expose port 80
+			Networks: []string{networkName, "bar"},
+		}
+
+		// define empty inputs to be overwritten by the pre create hook
+		inputConfig := &container.Config{
+			Image: def.Image,
+		}
+		inputHostConfig := &container.HostConfig{}
+		inputNetworkingConfig := &network.NetworkingConfig{}
+
+		err = preCreateContainerHook(ctx, dockerClient, def, inputConfig, inputHostConfig, inputNetworkingConfig)
+		require.NoError(t, err)
+
+		// assertions
+
+		require.Empty(
+			t,
+			inputNetworkingConfig.EndpointsConfig[networkName].Aliases,
+			"Networking config's aliases should be empty",
+		)
+		require.Equal(
+			t,
+			nw.ID,
+			inputNetworkingConfig.EndpointsConfig[networkName].NetworkID,
+			"Networking config's network ID should be retrieved from Docker",
+		)
+	})
+
+	t.Run("definition-contains-exposed-port-modifiers-without-protocol", func(t *testing.T) {
+		def := &Definition{
+			Image: "nginx:alpine", // alpine image does expose port 80
+			HostConfigModifier: func(hostConfig *container.HostConfig) {
+				hostConfig.PortBindings = nat.PortMap{
+					"80/tcp": []nat.PortBinding{
+						{
+							HostIP:   "localhost",
+							HostPort: "8080",
+						},
+					},
+				}
+			},
+			ExposedPorts: []string{"80"},
+		}
+
+		// define empty inputs to be overwritten by the pre create hook
+		inputConfig := &container.Config{
+			Image: def.Image,
+		}
+		inputHostConfig := &container.HostConfig{}
+		inputNetworkingConfig := &network.NetworkingConfig{}
+
+		err := preCreateContainerHook(ctx, dockerClient, def, inputConfig, inputHostConfig, inputNetworkingConfig)
+		require.NoError(t, err)
+
+		// assertions
+		require.Equal(t, "localhost", inputHostConfig.PortBindings["80/tcp"][0].HostIP)
+		require.Equal(t, "8080", inputHostConfig.PortBindings["80/tcp"][0].HostPort)
+	})
+
+	t.Run("definition-contains-exposed-port-modifiers-with-protocol", func(t *testing.T) {
+		def := &Definition{
+			Image: "nginx:alpine", // alpine image does expose port 80
+			HostConfigModifier: func(hostConfig *container.HostConfig) {
+				hostConfig.PortBindings = nat.PortMap{
+					"80/tcp": []nat.PortBinding{
+						{
+							HostIP:   "localhost",
+							HostPort: "8080",
+						},
+					},
+				}
+			},
+			ExposedPorts: []string{"80/tcp"},
+		}
+
+		// define empty inputs to be overwritten by the pre create hook
+		inputConfig := &container.Config{
+			Image: def.Image,
+		}
+		inputHostConfig := &container.HostConfig{}
+		inputNetworkingConfig := &network.NetworkingConfig{}
+
+		err := preCreateContainerHook(ctx, dockerClient, def, inputConfig, inputHostConfig, inputNetworkingConfig)
+		require.NoError(t, err)
+
+		// assertions
+		require.Equal(t, "localhost", inputHostConfig.PortBindings["80/tcp"][0].HostIP)
+		require.Equal(t, "8080", inputHostConfig.PortBindings["80/tcp"][0].HostPort)
+	})
+}
+
+func testCreateNetwork(t *testing.T, networkName string) network.CreateResponse {
+	t.Helper()
+
+	dockerClient, err := dockerclient.New(context.Background())
+	require.NoError(t, err)
+
+	nw, err := dockerClient.Client().NetworkCreate(context.Background(), networkName, network.CreateOptions{
+		Driver: "bridge",
+	})
+	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		err := dockerClient.Client().NetworkRemove(context.Background(), nw.ID)
+		require.NoError(t, err)
+		require.NoError(t, dockerClient.Close())
+	})
+
+	return nw
 }
