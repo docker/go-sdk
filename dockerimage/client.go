6f2ee87839a686806e20e90b7b8baa508e386487
Manuel de la Peña <mdelapenya@gmail.com>
2025-06-09 16:32:23 +0200

    chore: simplify pull interface

--- dockerimage/client.go
+++ dockerimage/client.go
@@ -6,9 +6,6 @@ import (
 
 // ImageClient is a client to perform operations on images.
 type ImageClient interface {
-	// Close closes the client.
-	Close() error
-
 	// Logger returns the logger.
 	Logger() *slog.Logger
 }
