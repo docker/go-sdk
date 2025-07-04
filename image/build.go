c530b3728d032f1aa3fb95705566fdf50b06e352
Manuel de la Peña <mdelapenya@gmail.com>
2025-07-04 10:49:32 +0200

    fix: use client's log for the build log

--- image/build.go
+++ image/build.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"os"
 	"path/filepath"
 	"time"
 
@@ -87,7 +86,6 @@ func Build(ctx context.Context, contextReader io.Reader, tag string, opts ...Bui
 	}
 
 	buildOpts := &buildOptions{
-		logWriter: os.Stdout,
 		opts: build.ImageBuildOptions{
 			Dockerfile: "Dockerfile",
 		},
@@ -154,7 +152,8 @@ func Build(ctx context.Context, contextReader io.Reader, tag string, opts ...Bui
 	}
 	defer resp.Body.Close()
 
-	output := buildOpts.logWriter
+	// use the bridge to log to the client logger
+	output := &loggerWriter{logger: buildOpts.buildClient.Logger()}
 
 	// Always process the output, even if it is not printed
 	// to ensure that errors during the build process are
