a5e8fc285196a6db5a477c7feefbd2d3a7977acc
Manuel de la Peña <mdelapenya@gmail.com>
2025-07-10 14:43:27 +0200

    feat(volume): find volumes

--- volume/options.go
+++ volume/options.go
@@ -1,6 +1,7 @@
 package volume
 
 import (
+	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/go-sdk/client"
 )
 
@@ -47,6 +48,31 @@ type terminateOptions struct {
 func WithForce() TerminateOption {
 	return func(o *terminateOptions) error {
 		o.force = true
+		return nil
+	}
+}
+
+type findOptions struct {
+	dockerClient *client.Client
+	filters      filters.Args
+}
+
+// FindOptions is a function that modifies the find options
+// used to find volumes.
+type FindOptions func(opts *findOptions) error
+
+// WithFindClient returns an [FindOptions] that sets the find client.
+func WithFindClient(dockerClient *client.Client) FindOptions {
+	return func(o *findOptions) error {
+		o.dockerClient = dockerClient
+		return nil
+	}
+}
+
+// WithFilters sets the filters to be used to filter the volumes.
+func WithFilters(filters filters.Args) FindOptions {
+	return func(opts *findOptions) error {
+		opts.filters = filters
 		return nil
 	}
 }
