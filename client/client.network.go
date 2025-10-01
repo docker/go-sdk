5c4578afa1b2ebb3935064bbc3fcc16ca4b98060
Manuel de la Peña <mdelapenya@gmail.com>
2025-10-01 12:01:19 +0200

    Revert "feat: add missing network methods"
    This reverts commit 7cdba5a0ab7c9cccd071f759453e9671af99757a.

--- client/client.network.go
+++ client/client.network.go
@@ -3,7 +3,6 @@ package client
 import (
 	"context"
 
-	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/api/types/network"
 )
 
@@ -13,24 +12,4 @@ func (c *sdkClient) NetworkCreate(ctx context.Context, name string, options netw
 	AddSDKLabels(options.Labels)
 
 	return c.APIClient.NetworkCreate(ctx, name, options)
-}
-
-// NetworkDisconnect disconnects a container from a network
-func (c *Client) NetworkDisconnect(ctx context.Context, networkID, containerID string, force bool) error {
-	dockerClient, err := c.Client()
-	if err != nil {
-		return fmt.Errorf("docker client: %w", err)
-	}
-
-	return dockerClient.NetworkDisconnect(ctx, networkID, containerID, force)
-}
-
-// NetworksPrune deletes unused networks
-func (c *Client) NetworksPrune(ctx context.Context, pruneFilters filters.Args) (network.PruneReport, error) {
-	dockerClient, err := c.Client()
-	if err != nil {
-		return network.PruneReport{}, fmt.Errorf("docker client: %w", err)
-	}
-
-	return dockerClient.NetworksPrune(ctx, pruneFilters)
 }
