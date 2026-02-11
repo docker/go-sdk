138ac029b267b231b204d28747486b4ff9cef0b0
xHozey <hamzasaliheddine@hotmail.com>
2026-02-11 13:16:11 +0100

    chore(client): add example for FindContainerByName

--- client/examples_test.go
+++ client/examples_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/moby/moby/api/types/container"
 	dockerclient "github.com/moby/moby/client"
 
 	"github.com/docker/go-sdk/client"
@@ -27,4 +28,35 @@ func ExampleNew() {
 
 	// Output:
 	// true
+}
+
+func ExampleSDKClient_FindContainerByName() {
+	cli, err := client.New(context.Background())
+	if err != nil {
+		log.Printf("error creating client: %s", err)
+		return
+	}
+
+	_, err = cli.ContainerCreate(context.Background(), dockerclient.ContainerCreateOptions{
+		Config: &container.Config{
+			Image: "nginx:alpine",
+		},
+		Name: "container-by-name-example",
+	})
+	defer func() {
+		if _, err := cli.ContainerRemove(context.Background(), "container-by-name-example", dockerclient.ContainerRemoveOptions{Force: true}); err != nil {
+			log.Printf("error removing container: %s", err)
+		}
+	}()
+
+	container, err := cli.FindContainerByName(context.Background(), "container-by-name-example")
+	if err != nil {
+		log.Printf("error finding container: %s", err)
+		return
+	}
+
+	fmt.Println(container.Names[0])
+
+	// Output:
+	// /container-by-name-example
 }
